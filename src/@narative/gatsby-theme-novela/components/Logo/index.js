c6f162e07d86ce1c61597c35a0273ec6b211558c
Ivy Chen <ichen9@ucsc.edu>
2021-03-16 16:15:27 +0800

    changed size

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -10,7 +10,7 @@ import React from 'react';
 export default function Logo({ fill }) {
   return (
     <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-        width="90.000000pt" height="90.000000pt" viewBox="0 0 90.000000 90.000000"
+        width="35.000000pt" height="35.000000pt" viewBox="0 0 90.000000 90.000000"
         preserveAspectRatio="xMidYMid meet">
 
         <g transform="translate(0.000000,90.000000) scale(0.100000,-0.100000)"
